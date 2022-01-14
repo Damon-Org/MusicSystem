b53321ee542d0779d6e60403dd72906aef13e8b4
Yimura <andreas.maerten@scarlet.be>
2022-01-14 01:37:59 +0100

    feat(Commands): Added volume default command

---
+++ commands/VolumeDefault.js
@@ -0,0 +1,50 @@
+import Modules from '@/src/Modules.js'
+
+export default class VolumeDefault extends Modules.commandRegistrar.BaseCommand {
+    /**
+     * @param {string} category
+     * @param {Main} main
+     */
+    constructor(category, main) {
+        super(main);
+
+        this.register(VolumeDefault, {
+            category: category,
+            guild_only: true,
+
+            name: 'volume default',
+            aliases: [],
+            description: 'Set the default volume level for the bot every time it joins.',
+            usage: 'volume default [volume]',
+            params: [
+                {
+                    name: 'volume',
+                    description: 'New default volume level.',
+                    type: 'int',
+                    default: 'Set the volume back to its default volume.'
+                }
+            ],
+            example: 'volume default 50'
+        });
+    }
+
+    /**
+     * @param {string} trigger string representing what triggered the command
+     */
+    async run(trigger) {
+        const volume = this.get('volume');
+        if (volume && volume > 5 && volume < 200) {
+            await this.server.settings.update({ music_system: { volume } });
+
+            this.send(`The default volume has been set to ${volume}, use \`d!volume default\` to set the old volume level back.`);
+
+            return true;
+        }
+
+        await this.server.settings.update({ music_system: { volume: Modules.music.constants.SystemReset.volume } });
+
+        this.send('No default volume given, the default volume has been set to 30.');
+
+        return true;
+    }
+}
